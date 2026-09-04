c21217a703afb0632ceccdf7c25290fd31281022
Prabhat <workforprabhat1254@gmail.com>
2026-09-04 12:38:08 +0530

    init: web app, with tailwind, shadcn and base setup

---
+++ app/web/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function Home() {
+  return (
+    <main className="space-y-4 p-4">
+      <h3 className="text-5xl">Testing shit!</h3>
+      <Button>Login</Button>
+    </main>
+  );
+}
+
+export default Home;
